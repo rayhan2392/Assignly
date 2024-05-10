521af342a2a274c98d1ebe08d11d866eaf3f00e2
rkshawn <rkshawn975@gmail.com>
2024-05-10 21:04:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Explore the world</button>
+    </>
+  );
+}
+
+export default App;
